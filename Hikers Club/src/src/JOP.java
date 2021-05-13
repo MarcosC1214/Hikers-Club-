685cd35a97dd0114530fde22345450febfe55970
Marcos Carrillo <83974878+0373067@users.noreply.github.com>
2021-05-12 21:15:34 -0400

    Update JOP.java

--- Hikers Club/src/src/JOP.java
+++ Hikers Club/src/src/JOP.java
@@ -2,13 +2,13 @@ package src;
 
 import javax.swing.JOptionPane;
 
-public class JOP {
+public class JOP { // Jop class in charge of all user imput and pop up messages. 
 
-	public static void msg(String msg) {
+	public static void msg(String msg) { // Message pop up only method.
 		JOptionPane.showMessageDialog(null, msg);
 	}
 	
-	public static String in(String msg){
+	public static String in(String msg) { // Message along with user input method.
 		return JOptionPane.showInputDialog(msg);
 	}
 }
